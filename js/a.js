f37c53311ce2a5f94848a72c0bca4ef2542b585d
tomaszjureczko <tomasz.jureczko@gmail.com>
2011-10-22 22:05:57 +0100

    tools and targets toold displayed

--- js/a.js
+++ js/a.js
@@ -2,12 +2,37 @@ cooking.displayLevel = function(recipe,step){
   
     document.getElementById('instruction').innerHTML = recipe.steps[step].instruction;
 
-    tools = recipe.steps[step].tools; 
+    tools = recipe.steps[step].tools;
+    var toolElement;
     for (var i=0, len=tools.length; i<len; ++i ){
-        console.dir(tools[i]);
+        // tool
+        toolEl = document.createElement('img');
+        toolEl.setAttribute('src', tools[i].image);
+        toolEl.setAttribute('class', 'tool');
+        toolEl.setAttribute('id',tools[i].id);
+        toolEl.style.top = tools[i].start.y + '%';  
+        toolEl.style.left = tools[i].start.x + '%';  
+        toolEl.style.width = tools[i].size.x + "%" ;  
+        toolEl.style.height = tools[i].size.y + '%';  
+        document.body.appendChild(toolEl);
+
+        // target for tool
+        toolEl = document.createElement('div');
+        toolEl.setAttribute('class', 'toolTarget');
+        toolEl.setAttribute('id',tools[i].id + "Target");
+        toolEl.style.top = tools[i].stop.y + '%';  
+        toolEl.style.left = tools[i].stop.x + '%';  
+        toolEl.style.width = tools[i].size.x + "%" ;  
+        toolEl.style.height = tools[i].size.y + '%';  
+        document.body.appendChild(toolEl);
     }
 
 }
 
-cooking.displayLevel(cooking.recipe1,0);
+cooking.gameStart = (function(){ 
+    cooking.viewportwidth = window.innerWidth,
+    cooking.viewportheight = window.innerHeight
+
+    cooking.displayLevel(cooking.recipe1,0);
+})();
 
--- js/recipe1.js
+++ js/recipe1.js
@@ -8,24 +8,42 @@ cooking.recipe1.steps = [];
 
 cooking.recipe1.steps[0] = {};
 
-cooking.recipe1.steps[0].instruction = "Prepare tools CUTTING BOARD, KNIFE, SMALL BOWL and DRIED APRICOT";
+cooking.recipe1.steps[0].instruction = "Prepare tools KNIFE, SMALL BOWL";
 
 cooking.recipe1.steps[0].tools = [];
 
 cooking.recipe1.steps[0].tools[0] = {
-        id : "cuttingboard",
-        displayName : "cuttingboard",
-        size : {
-            "x" : "3",
-            "y" : "5"
-        },
-        stop : {
-            "x" : "40",
-            "y" : "90"
-        },
-        start : {
-            "x" : "10",
-            "y" : "10"
-        },
-        image: "/img/cuttingboard.gif"
-    }
+    id : "cuttingboard",
+    displayName : "Cutting Board",
+    size : {
+        "x" : "16",
+        "y" : "15"
+    },
+    stop : {
+        "x" : "40",
+        "y" : "80"
+    },
+    start : {
+        "x" : "50",
+        "y" : "44"
+    },
+    image: "img/deska.png"
+}
+
+cooking.recipe1.steps[0].tools[1] = {
+    id : "driedapricot",
+    displayName : "Dried Apricot",
+    size : {
+        "x" : "6",
+        "y" : "13"
+    },
+    stop : {
+        "x" : "40",
+        "y" : "60"
+    },
+    start : {
+        "x" : "2",
+        "y" : "8"
+    },
+    image: "img/apricot.png"
+}
